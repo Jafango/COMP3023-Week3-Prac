6848c9f0ee1a4c20aa7b92a732b272cb3abb15a9
Joshua Froggatt <jfrogg.au@gmail.com>
2024-03-12 12:35:46 +1030

    modified:   Week 3 Prac.cpp Finished Activity 1

--- Week 3 Prac.cpp
+++ Week 3 Prac.cpp
@@ -5,8 +5,43 @@
 
 using namespace std;
 
+class Pie {
+	public:
+		virtual string description() = 0;
+};
+
+class ApricotPie : public Pie {
+	public:
+		string description() 
+		{
+			return "Apricot Pie";
+		}
+};
+
+class ApplePie : public Pie {
+public:
+	string description()
+	{
+		return "Apple Pie";
+	}
+};
+
+class RaspberryPie : public Pie {
+public:
+	string description()
+	{
+		return "Raspberry Pie";
+	}
+};
+
 int main()
 {
-	cout << "Hello CMake." << endl;
+	ApplePie ap;
+	ApricotPie acp;
+	RaspberryPie rp;
+	std::cout << ap.description() << std::endl;
+	std::cout << acp.description() << std::endl;
+	std::cout << rp.description() << std::endl;
 	return 0;
 }
+
