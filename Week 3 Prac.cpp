38ba6684acfd8c0d50d610610652eb7b2ec09e53
Joshua Froggatt <jfrogg.au@gmail.com>
2024-03-12 12:12:15 +1030

    new file:   .gitignore 	new file:   CMakeLists.txt 	new file:   CMakePresets.json 	new file:   Week 3 Prac.cpp 	new file:   Week 3 Prac.h Init commit

---
+++ Week 3 Prac.cpp
@@ -0,0 +1,12 @@
+﻿// Week 3 Prac.cpp : Defines the entry point for the application.
+//
+
+#include "Week 3 Prac.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
